cad8672ca14d596ef0cbe53e08eb5082631d1d61
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-23 14:17:16 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="viga">
+        <div className="contenedor-semaforo">
+          <div className="rojo luz"></div>
+          <div className="amarillo luz"></div>
+          <div className="verde luz"></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
